5875bc609b2ce1bb103aab3687134e7624dd4297
meysam-mfard <mmohagheghi@gmail.com>
2019-07-10 17:50:03 +0200

    Minor fix;

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 import classes from './Button.module.css';
 
-const button = (props) => {
+const button = (props) => (
   <button className={[classes.Button, classes[props.btnType]].join(' ')}
-    onClick={props.clicked}>
+          onClick={props.clicked}>
     {props.children}
   </button>
-};
+);
 
 export default button;
