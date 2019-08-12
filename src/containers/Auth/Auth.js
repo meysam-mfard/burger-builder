250c02a61ec1f1dc86bebbf483b21538031ddd48
meysam-mfard <mmohagheghi@gmail.com>
2019-08-12 11:38:52 +0200

    Add authentication actions.

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,11 +2,13 @@ import React, {Component} from 'react';
 import Input from "../../components/UI/Input/Input";
 import classes from './Auth.module.css';
 import Button from "../../components/UI/Button/Button";
+import {connect} from "react-redux";
+import * as actions from '../../store/actions/index';
 
 class Auth extends Component {
     state = {
         authForm: {
-            username: {
+            email: {
                 elementType: 'input',
                 elementConfig: {
                     type: 'email',
@@ -77,6 +79,11 @@ class Auth extends Component {
         this.setState({authForm: updatedAuthForm});
     };
 
+    submitHandler = (event) => {
+        event.preventDefault();
+        this.props.onAuth(this.state.authForm.email.value, this.state.authForm.password.value);
+    };
+
     render() {
 
         const formElementArray = [];
@@ -101,7 +108,7 @@ class Auth extends Component {
 
         return (
             <div className={classes.Auth}>
-                <form>
+                <form onSubmit={this.submitHandler}>
                     {formElements}
                     <Button btnType="Success">SUBMIT</Button>
                 </form>
@@ -111,4 +118,10 @@ class Auth extends Component {
 
 }
 
-export default Auth;
+const mapDispatchToProps = dispatch => {
+    return {
+        onAuth: (email, password) => dispatch(actions.auth(email, password))
+    };
+};
+
+export default connect(null, mapDispatchToProps)(Auth);
