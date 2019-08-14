f5d946ba7021439cb868f66d070819cd6b7fa97c
meysam-mfard <mmohagheghi@gmail.com>
2019-08-14 12:36:52 +0200

    Add Logout link.

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -7,7 +7,9 @@ const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
       <NavigationItem link="/" exact>Burger Builder</NavigationItem>
       <NavigationItem link="/orders">Orders</NavigationItem>
-      <NavigationItem link="/auth">Authentication</NavigationItem>
+      {props.isAuthenticated
+          ? <NavigationItem link="/logout">Logout</NavigationItem>
+          : <NavigationItem link="/auth">Authentication</NavigationItem>}
     </ul>
 );
 
--- src/containers/Layout/Layaout.js
+++ src/containers/Layout/Layaout.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {connect} from "react-redux";
 
 import classes from './Layout.module.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
@@ -6,31 +7,42 @@ import Sidedrawer from '../../components/Navigation/Sidedrawer/Sidedrawer';
 
 class Layout extends Component {
 
-  state = {
-    showSidedrawer: false
-  };
+    state = {
+        showSidedrawer: false
+    };
 
-  sidedrawerCloseHandler = () => {
-    this.setState({showSidedrawer: false});
-  };
+    sidedrawerCloseHandler = () => {
+        this.setState({showSidedrawer: false});
+    };
 
-  sidedrawerToggleHandler = () => {
-    this.setState((prevState) => {
-      return {showSidedrawer: !prevState.showSidedrawer}
-    } );
-  };
+    sidedrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return {showSidedrawer: !prevState.showSidedrawer}
+        });
+    };
 
-  render() {
-    return (
-      <>
-        <Sidedrawer open={this.state.showSidedrawer} close={this.sidedrawerCloseHandler}/>
-        <Toolbar drawerToggleClicked={this.sidedrawerToggleHandler}/>
-        <main className={classes.content}>
-          {this.props.children}
-        </main>
-      </>
-    )
-  }
+    render() {
+        return (
+            <>
+                <Sidedrawer
+                    isAuth={this.props.isAuthenticated}
+                    open={this.state.showSidedrawer}
+                    close={this.sidedrawerCloseHandler}/>
+                <Toolbar
+                    isAuth={this.props.isAuthenticated}
+                    drawerToggleClicked={this.sidedrawerToggleHandler}/>
+                <main className={classes.content}>
+                    {this.props.children}
+                </main>
+            </>
+        )
+    }
 }
 
-export default Layout;
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.token !== null
+    }
+};
+
+export default connect(mapStateToProps)(Layout);
