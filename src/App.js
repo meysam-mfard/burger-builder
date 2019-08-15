14b5255bfe07503f1ba2502fc23eaf9dc3006d7a
meysam-mfard <mmohagheghi@gmail.com>
2019-08-15 18:30:59 +0200

    Persist authentication state in localStorage.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, withRouter} from 'react-router-dom';
+import {connect} from 'react-redux';
 
 import Layout from './containers/Layout/Layaout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -7,8 +8,13 @@ import Checkout from "./containers/Checkout/Checkout";
 import Orders from "./containers/Orders/Orders";
 import Auth from "./containers/Auth/Auth";
 import Logout from "./containers/Auth/Logout/Logout";
+import * as actions from './store/actions/index';
 
 class App extends Component {
+    componentDidMount() {
+        this.props.autoSignin();
+    }
+
     render() {
         return (
             <div>
@@ -27,4 +33,12 @@ class App extends Component {
     }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+    return {
+        autoSignin: () => dispatch(actions.authCheckState())
+    };
+};
+
+export default withRouter(connect(null, mapDispatchToProps)(App));
+//withRouter is added since after wrapping App with connect, it does not receive props from react-route
+// only needed for components that are not a “route component”, not rendered with: <Route component={}/>
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,6 +23,9 @@ export const authFail = (error) => {
 };
 
 export const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    localStorage.removeItem('expirationDate');
     return {
         type: actionTypes.AUTH_LOGOUT
     }
@@ -58,6 +61,10 @@ export const auth = (email, password, isSignUp) => {
         }
         axios.post(url, authData)
             .then(response => {
+                const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+                localStorage.setItem('token', response.data.idToken);
+                localStorage.setItem('userId', response.data.localId);
+                localStorage.setItem('expirationDate', expirationDate);
                 dispatch(authSuccess(response.data.idToken, response.data.localId));
                 dispatch(checkAuthTimeout(response.data.expiresIn));
             })
@@ -65,4 +72,22 @@ export const auth = (email, password, isSignUp) => {
                 dispatch(authFail(error.response.data.error));
             });
     };
+};
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            dispatch(logout());
+        } else {
+            const expirationDate = new Date(localStorage.getItem('expirationDate'));
+            if (new Date() >= expirationDate) {
+                dispatch(logout());
+            } else {
+                dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime()) / 1000));
+                dispatch(authSuccess(token, localStorage.getItem('userId')));
+            }
+        }
+    }
+
 };
