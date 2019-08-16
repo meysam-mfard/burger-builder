914fd23c7136ede468629578f3524f3376fea6c7
meysam-mfard <mmohagheghi@gmail.com>
2019-08-16 09:47:02 +0200

    Guarding routes.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Route, Switch, withRouter} from 'react-router-dom';
+import {Route, Switch, withRouter, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 
 import Layout from './containers/Layout/Layaout';
@@ -16,16 +16,33 @@ class App extends Component {
     }
 
     render() {
+
+        let routes = (
+            <>
+                <Route path={'/'} exact component={BurgerBuilder}/>
+                <Route path={'/auth'} component={Auth}/>
+                <Redirect to={'/'}/>
+            </>
+        );
+
+        if (this.props.isAuthenticated) {
+            routes = (
+                <>
+                    <Route path={'/logout'} component={Logout}/>
+                    <Route path={'/checkout'} component={Checkout}/>
+                    <Route path={'/orders'} component={Orders}/>
+                    <Route path={'/'} exact component={BurgerBuilder}/>
+                    <Redirect to={'/'}/>
+                </>
+            );
+        }
+
         return (
             <div>
                 <Layout>
                     <Switch>
-                        <Route path={'/'} exact component={BurgerBuilder}/>
-                        <Route path={'/auth'} component={Auth}/>
-                        <Route path={'/logout'} component={Logout}/>
-                        <Route path={'/checkout'} component={Checkout}/>
-                        <Route path={'/orders'} component={Orders}/>
-                        <Route render={() => <h1 style={{color: 'red', textAlign: 'center'}}>Page Not Found</h1>}/>
+                        {routes}
+                        {/*<Route render={() => <h1 style={{color: 'red', textAlign: 'center'}}>Page Not Found</h1>}/>*/}
                     </Switch>
                 </Layout>
             </div>
@@ -33,12 +50,18 @@ class App extends Component {
     }
 }
 
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.token !== null
+    };
+};
+
 const mapDispatchToProps = dispatch => {
     return {
         autoSignin: () => dispatch(actions.authCheckState())
     };
 };
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
 //withRouter is added since after wrapping App with connect, it does not receive props from react-route
 // only needed for components that are not a “route component”, not rendered with: <Route component={}/>
